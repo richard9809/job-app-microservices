8672e1091745c4b3ec5e591e4ed88cafdb1fd470
rmulu-dast <rmulu@DAST.CO>
2025-06-18 12:47:05 -0700

    Feat: ConfigServer, Company, Gateway, Job, Review, Service-reg MS

---
+++ jobms/src/main/java/com/embarkx/jobms/job/impl/JobServiceImpl.java
@@ -0,0 +1,111 @@
+package com.embarkx.jobms.job.impl;
+
+import com.embarkx.jobms.job.Job;
+import com.embarkx.jobms.job.JobRepository;
+import com.embarkx.jobms.job.JobService;
+import com.embarkx.jobms.job.clients.CompanyClient;
+import com.embarkx.jobms.job.clients.ReviewClient;
+import com.embarkx.jobms.job.dto.JobDTO;
+import com.embarkx.jobms.job.external.Company;
+import com.embarkx.jobms.job.external.Review;
+import com.embarkx.jobms.job.mapper.JobMapper;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class JobServiceImpl implements JobService {
+    //private final List<Job> jobs = new ArrayList<>();
+    //private Long nextId = 1L;
+
+    JobRepository jobRepository;
+
+    int attempt = 0;
+
+//    @Autowired
+//    RestTemplate restTemplate;
+
+    private final CompanyClient companyClient;
+    private final ReviewClient reviewClient;
+
+    public JobServiceImpl(JobRepository jobRepository, CompanyClient companyClient, ReviewClient reviewClient) {
+        this.jobRepository = jobRepository;
+        this.companyClient = companyClient;
+        this.reviewClient  = reviewClient;
+    }
+
+    @Override
+//    @CircuitBreaker(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+//    @Retry(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+    @RateLimiter(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+    public List<JobDTO> findAll() {
+        System.out.println("Attempt: "+ ++attempt);
+        return jobRepository.findAll().stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> companyBreakerFallback(Exception e) {
+        List<String> list = new ArrayList<>();
+        list.add("Dummy");
+        return list;
+    }
+
+    private JobDTO convertToDto(Job job) {
+        // getForObject is useful for when you know the response is a single object
+        Company company = companyClient.getCompany(job.getCompanyId());
+        List<Review> reviews = reviewClient.getReviews(job.getCompanyId());
+
+        JobDTO jobDTO = JobMapper.mapToJobWithCompanyDto(job, company, reviews);
+
+        return jobDTO;
+    }
+
+    @Override
+    public void createJob(Job job) {
+        jobRepository.save(job);
+    }
+
+    @Override
+    public JobDTO getJobById(Long id) {
+        Job job = jobRepository.findById(id).orElse(null);
+        return convertToDto(job);
+    }
+
+    @Override
+    public boolean deleteJobById(Long id) {
+        try {
+            jobRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean updateJob(Long id, Job updatedJob) {
+        Optional<Job> jobOptional = jobRepository.findById(id);
+        if (jobOptional.isPresent()) {
+            Job job = jobOptional.get();
+            job.setTitle(updatedJob.getTitle());
+            job.setDescription(updatedJob.getDescription());
+            job.setMinSalary(updatedJob.getMinSalary());
+            job.setMaxSalary(updatedJob.getMaxSalary());
+            job.setLocation(updatedJob.getLocation());
+            jobRepository.save(job);
+            return true;
+        }
+        return false;
+    }
+}
